9c6caea3e5f6561c6d87fd6742e48139ed4681b5
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-07-31 19:17:27 +0300

    setup file and folder structure

---
+++ src/components/CountriesList.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CountriesList = () => {
+    return (
+        <h1>Countries List</h1>
+    )
+};
+
+export default CountriesList;
---
+++ src/pages/CoutriesDetailsPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CountriesDetailsPage = () => {
+    return (
+        <h1>Home page</h1>
+    )
+};
+
+export default CountriesDetailsPage;
