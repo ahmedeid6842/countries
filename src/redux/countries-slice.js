639b2cbfe769bf93bebdae971c06c6ba8da293df
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-08-01 05:17:50 +0300

    Added the countries redux slice

--- src/redux/countries-slice.js
+++ src/redux/countries-slice.js
@@ -11,3 +11,47 @@ export const getCountry = createAsyncThunk('countries/getCountry', async (name) 
   const response = await axios.get(`https://restcountries.com/v3.1/name/${name}`);
   return response.data[0];
 });
+
+const countriesSlice = createSlice({
+  name: 'countries',
+  initialState: {
+    countries: [],
+    country: null,
+    status: 'idle',
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getCountries.pending, (state) => ({
+        ...state,
+        status: 'loading',
+      }))
+      .addCase(getCountries.fulfilled, (state, action) => ({
+        ...state,
+        status: 'succeeded',
+        countries: action.payload,
+      }))
+      .addCase(getCountries.rejected, (state, action) => ({
+        ...state,
+        status: 'failed',
+        error: action.error.message,
+      }))
+      .addCase(getCountry.pending, (state) => ({
+        ...state,
+        status: 'loading',
+      }))
+      .addCase(getCountry.fulfilled, (state, action) => ({
+        ...state,
+        status: 'succeeded',
+        country: action.payload,
+      }))
+      .addCase(getCountry.rejected, (state, action) => ({
+        ...state,
+        status: 'failed',
+        error: action.error.message,
+      }));
+  },
+});
+
+export default countriesSlice.reducer;
